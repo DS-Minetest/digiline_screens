7f2f260b3469b0c3af1fb80a7a31e0669f17388a
DS-Minetest <vorunbekannt75@web.de>
2017-03-01 17:25:52 +0100

    generalize screen registration

--- init.lua
+++ init.lua
@@ -14,18 +14,12 @@ digiline_screens = {}
 
 digiline_screens.registered_screens = {}
 
---~ function digiline_screens.register_screen(name, spec, width, high)
-	--~ minetest.register_node(name, spec)
-	--~ digiline_screens.registered_screens[name] = minetest.registered_nodes[name]
---~ end
-
---~ digiline_screens.register_screen("digiline_screens:screen",
-	--~ {
-		--~ description = "screen",
-	--~ },
-	--~ 16,
-	--~ 16
---~ )
+minetest.register_entity("digiline_screens:entity", {
+	collisionbox = {0,0,0, 0,0,0},
+	physical = false,
+	visual = "upright_sprite",
+	textures = {"blank.png"},
+})
 
 local function make_texture(base, t, w, h)
 	local px = "digiline_screens_px.png"
@@ -54,26 +48,83 @@ local function make_texture(base, t, w, h)
 	return tex
 end
 
-local function on_digiline_receive(pos, node, channel, msg)
-	local meta = minetest.get_meta(pos)
-	local setchan = meta:get_string("channel")
-	if setchan ~= channel then
-		return
+function digiline_screens.register_screen(name, spec, width, hight, entposs)
+	spec.digiline = {receptor, effector}
+	spec.digiline.effector = spec.digiline.effector or {}
+	local f = spec.digiline.effector.action
+	spec.digiline.effector.action = function(pos, node, channel, msg)
+		if f then f(pos, node, channel, msg) end
+		local meta = minetest.get_meta(pos)
+		local setchan = meta:get_string("channel")
+		if setchan ~= channel then
+			return
+		end
+		if type(msg) ~= "table" then
+			return
+		end
+		local ent
+		for _,obj in pairs(minetest.get_objects_inside_radius(pos, 0.5)) do
+			local lent = obj:get_luaentity()
+			if lent and lent.name == "digiline_screens:entity" then
+				ent = obj
+				break
+			end
+		end
+		ent:set_properties({
+				textures={make_texture(spec.tiles[1], msg, 16, 16)}
+			})
 	end
-	if type(msg) ~= "table" then
-		return
-	end
-	local ent
-	for _,obj in pairs(minetest.get_objects_inside_radius(pos, 0.5)) do
-		local lent = obj:get_luaentity()
-		if lent and lent.name == "digiline_screens:entity" then
-			ent = obj
-			break
+
+	local f = spec.after_place_node
+	spec.after_place_node = function(pos, placer, itemstack)
+		if f then f(pos, placer, itemstack) end
+		if spec.paramtype2 == "wallmounted" then
+			local entpos = entposs[minetest.get_node(pos).param2]
+			if entpos == nil then return end
+			local ent = minetest.add_entity(
+					vector.add(pos, entpos.delta),
+					"digiline_screens:entity")
+			ent:setyaw(entpos.yaw or 0)
+		else
+			minetest.add_entity(pos, "digiline_screens:entity")
 		end
 	end
-	ent:set_properties({
-			textures={make_texture("digiline_screens_screen.png", msg, 16, 16)}
-		})
+
+	local f = spec.on_construct
+	spec.on_construct = function(pos)
+		if f then f(pos) end
+		minetest.get_meta(pos):set_string("formspec",
+				"field[channel;Channel;${channel}]")
+	end
+
+	local f = spec.on_destruct
+	spec.on_destruct = function(pos)
+		if f then f(pos) end
+		for _,obj in pairs(minetest.get_objects_inside_radius(pos, 0.5)) do
+			local lent = obj:get_luaentity()
+			if lent and lent.name == "digiline_screens:entity" then
+				obj:remove()
+				break
+			end
+		end
+	end
+
+	local f = spec.on_receive_fields
+	spec.on_receive_fields = function(pos, formname, fields, player)
+		if f then f(pos, formname, fields, player) end
+		local name = player:get_player_name()
+		if minetest.is_protected(pos, name) and not
+				minetest.check_player_privs(name, {protection_bypass=true}) then
+			minetest.record_protection_violation(pos, name)
+			return
+		end
+		if fields.channel then
+			minetest.get_meta(pos):set_string("channel", fields.channel)
+		end
+	end
+
+	minetest.register_node(name, spec)
+	digiline_screens.registered_screens[name] = minetest.registered_nodes[name]
 end
 
 local entposs = {
@@ -88,76 +139,33 @@ local box = {
 	wall_top = {-8/16, 7/16, -8/16, 8/16, 8/16, 8/16}
 }
 
-minetest.register_node("digiline_screens:screen", {
-	description = "digiline screen",
-	drawtype = "nodebox",
-	inventory_image = "digiline_screens_screen.png",
-	wield_image = "digiline_screens_screen.png",
-	tiles = {"digiline_screens_screen.png"},
-	paramtype = "light",
-	sunlight_propagates = true,
-	paramtype2 = "wallmounted",
-	node_box = box,
-	selection_box = box,
-	groups = {choppy = 3, dig_immediate = 2},
-	digiline = {
-		receptor = {},
-		effector = {
-			action = on_digiline_receive
-		},
-	},
-	light_source = 6,
+digiline_screens.register_screen("digiline_screens:screen",
+	{
+		description = "digiline screen",
+		drawtype = "nodebox",
+		inventory_image = "digiline_screens_screen.png",
+		wield_image = "digiline_screens_screen.png",
+		tiles = {"digiline_screens_screen.png"},
+		paramtype = "light",
+		sunlight_propagates = true,
+		paramtype2 = "wallmounted",
+		node_box = box,
+		selection_box = box,
+		groups = {choppy = 3, dig_immediate = 2},
+		light_source = 6,
 
-	after_place_node = function (pos, placer, itemstack)
-		local param2 = minetest.get_node(pos).param2
-		if param2 == 0 or param2 == 1 then
-			minetest.add_node(pos, {name = "digiline_screens:screen", param2 = 3})
-			param2 = 3
-		end
-		local entpos = entposs[param2]
-		if entpos == nil then return end
-		local ent = minetest.add_entity(
-				{x = pos.x + entpos.delta.x,
-				y = pos.y + entpos.delta.y,
-				z = pos.z + entpos.delta.z},
-				"digiline_screens:entity")
-		ent:setyaw(entpos.yaw or 0)
-	end,
-
-	on_construct = function(pos)
-		minetest.get_meta(pos):set_string("formspec",
-				"field[channel;Channel;${channel}]")
-	end,
-
-	on_destruct = function(pos)
-		for _,obj in pairs(minetest.get_objects_inside_radius(pos, 0.5)) do
-			local lent = obj:get_luaentity()
-			if lent and lent.name == "digiline_screens:entity" then
-				obj:remove()
-				break
+		after_place_node = function (pos, placer, itemstack)
+			local param2 = minetest.get_node(pos).param2
+			if param2 == 0 or param2 == 1 then
+				minetest.add_node(pos, {name = "digiline_screens:screen", param2 = 3})
 			end
-		end
-	end,
+		end,
+	},
 
-	on_receive_fields = function(pos, formname, fields, player)
-		local name = player:get_player_name()
-		if minetest.is_protected(pos, name) and not
-				minetest.check_player_privs(name, {protection_bypass=true}) then
-			minetest.record_protection_violation(pos, name)
-			return
-		end
-		if fields.channel then
-			minetest.get_meta(pos):set_string("channel", fields.channel)
-		end
-	end,
-})
-
-minetest.register_entity("digiline_screens:entity", {
-	collisionbox = {0,0,0, 0,0,0},
-	physical=false,
-	visual = "upright_sprite",
-	textures = {"blank.png"},
-})
+	16,
+	16,
+	entposs
+)
 
 
 local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
